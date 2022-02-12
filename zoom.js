70aece9cd6435447ec870fe1159ef8214f6ac12a
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2022-02-12 09:22:30 +0200

    Update zoom.js

--- zoom.js
+++ zoom.js
@@ -1,62 +1,61 @@
 function magnify(imgID, zoom) {
-  let img1, img, glass, w, h;
-  img = document.getElementById(imgID);
-  img1 = document.getElementById("my1");
+    let img1, img, glass, w, h;
+    img = document.getElementById(imgID);
+    img1 = document.getElementById("my1");
+  
+    /*создать увеличительное стекло:*/
+    glass = document.createElement("DIV");
+    glass.setAttribute("class", "glass");
+  
+    /*вставить увеличительное стекло перед картинкой:*/
 
-  /*создать увеличительное стекло:*/
-  glass = document.createElement("DIV");
-  glass.setAttribute("class", "glass");
-
-  /*вставить увеличительное стекло перед картинкой:*/
-  // img1.parentElement.insertBefore(img, img1);
-  // img.parentElement.insertBefore(glass, img);
-  img.parentElement.insertBefore(glass, img);
-  /*установить стили для background для увеличительного стекла:*/
-  glass.style.backgroundImage = "url('" + img.src + "')";
-  glass.style.backgroundRepeat = "no-repeat";
-  glass.style.backgroundSize =
-    img.width * zoom + "px " + img.height * zoom + "px";
-
-  /*найти половину ширины и высоты стекла*/
-  w = glass.style.width / 2;
-  h = glass.style.height / 2;
-
-  /*обработка события перемещения мыши*/
-  glass.addEventListener("mousemove", moveMagnifier);
-  img.addEventListener("mousemove", moveMagnifier);
-
-  /*перемещение увеличительного стекла*/
-  function moveMagnifier(event) {
-    let x, y;
-
-    /*предотвратить действие по умолчанию*/
-    event.preventDefault();
-
-    /*получить координаты курсора*/
-    x = event.offsetX;
-    y = event.offsetY;
-
-    /*предотвратить позиционирование стекла далеко от картинки*/
-    if (x > img.width - w / zoom) {
-      x = img.width - w / zoom;
+    img.parentElement.insertBefore(glass, img);
+    /*установить стили для background для увеличительного стекла:*/
+    glass.style.backgroundImage = "url('" + img.src + "')";
+    glass.style.backgroundRepeat = "no-repeat";
+    glass.style.backgroundSize =
+      img.width * zoom + "px " + img.height * zoom + "px";
+  
+    /*найти половину ширины и высоты стекла*/
+    w = glass.style.width / 2;
+    h = glass.style.height / 2;
+  
+    /*обработка события перемещения мыши*/
+    glass.addEventListener("mousemove", moveMagnifier);
+    img.addEventListener("mousemove", moveMagnifier);
+  
+    /*перемещение увеличительного стекла*/
+    function moveMagnifier(event) {
+      let x, y;
+  
+      /*предотвратить действие по умолчанию*/
+      event.preventDefault();
+  
+      /*получить координаты курсора*/
+      x = event.offsetX;
+      y = event.offsetY;
+  
+      /*предотвратить позиционирование стекла далеко от картинки*/
+      if (x > img.width - w / zoom) {
+        x = img.width - w / zoom;
+      }
+      if (x < w / zoom) {
+        x = w / zoom;
+      }
+      if (y > img.height - h / zoom) {
+        y = img.height - h / zoom;
+      }
+      if (y < h / zoom) {
+        y = h / zoom;
+      }
+  
+    //   /*поменять позиционирование стекла по координатам мыши:*/
+    //   img.style.left = x - w + "px";
+    //   img.style.top = y - h + "px";
+  
+      /*позиционирование фона*/
+      glass.style.backgroundPosition =
+        "-" + (x * zoom - w) + "px -" + (y * zoom - h) + "px";
     }
-    if (x < w / zoom) {
-      x = w / zoom;
-    }
-    if (y > img.height - h / zoom) {
-      y = img.height - h / zoom;
-    }
-    if (y < h / zoom) {
-      y = h / zoom;
-    }
-
-    /*поменять позиционирование стекла по координатам мыши:*/
-    img.style.left = x - w + "px";
-    img.style.top = y - h + "px";
-
-    /*позиционирование фона*/
-    glass.style.backgroundPosition =
-      "-" + (x * zoom - w) + "px -" + (y * zoom - h) + "px";
   }
-}
-magnify("my", 3);
+  magnify("my", 3);
